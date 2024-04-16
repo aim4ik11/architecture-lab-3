868ed8a5ade8be6081447d4da41b475ca39313f8
Ivan Pasternak <retriumpf@gmail.com>
2024-04-16 20:34:13 +0300

    Yellow cross

--- ui/window.go
+++ ui/window.go
@@ -32,8 +32,8 @@ type Visualizer struct {
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
 	pw.pos.Max.Y = 200
+	pw.pos.Max.X = 200
 	driver.Main(pw.run)
 }
 
@@ -43,8 +43,11 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  800,
+		Height: 800,
 	})
+	w.Send(paint.Event{})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
 	}
@@ -132,7 +135,8 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
-
+	pw.w.Fill(image.Rect(200, 350, 600, 450), color.RGBA{255, 255, 0, 255}, draw.Src)
+	pw.w.Fill(image.Rect(350, 200, 450, 600), color.RGBA{255, 255, 0, 255}, draw.Src)
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
 
 	// Малювання білої рамки.
