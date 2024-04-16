a6256319a9f4cd3cae6c9e8d6fba3e7ddd7ae471
aim4ik <rusak.yaroslav@lll.kpi.ua>
2024-04-16 21:15:41 +0300

    task 1

--- ui/window.go
+++ ui/window.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
@@ -16,131 +17,165 @@ import (
 	"golang.org/x/mobile/event/size"
 )
 
+type Cross struct {
+  x    int
+  y    int
+  size int
+  w    int
+}
+
+func (c *Cross) drawCross(pw *Visualizer) {
+  x1 := c.x + c.size
+  y1 := c.y + c.size/2 + c.w/2
+  y2 := c.y + c.size/2 - c.w/2
+
+  pw.w.Fill(image.Rect(c.x, y1, x1, y2), color.RGBA{255, 255, 0, 255}, draw.Src)
+
+  x1 = c.x + c.size/2 + c.w/2
+  x2 := c.x + c.size/2 - c.w/2
+  y2 = c.y + c.size
+  pw.w.Fill(image.Rect(x1, c.y, x2, y2), color.RGBA{255, 255, 0, 255}, draw.Src)
+}
+
+func (c *Cross) move(x, y int) {
+  c.x = (x - c.size/2)
+  c.y = (y - c.size/2)
+}
+
+var (
+  cross = Cross{200, 200, 400, 100}
+)
+
 type Visualizer struct {
-	Title         string
-	Debug         bool
-	OnScreenReady func(s screen.Screen)
+  Title         string
+  Debug         bool
+  OnScreenReady func(s screen.Screen)
 
-	w    screen.Window
-	tx   chan screen.Texture
-	done chan struct{}
+  w    screen.Window
+  tx   chan screen.Texture
+  done chan struct{}
 
-	sz  size.Event
-	pos image.Rectangle
+  sz  size.Event
+  pos image.Rectangle
 }
 
 func (pw *Visualizer) Main() {
-	pw.tx = make(chan screen.Texture)
-	pw.done = make(chan struct{})
-	pw.pos.Max.Y = 200
-	pw.pos.Max.X = 200
-	driver.Main(pw.run)
+  pw.tx = make(chan screen.Texture)
+  pw.done = make(chan struct{})
+  pw.pos.Max.Y = 200
+  pw.pos.Max.X = 200
+  driver.Main(pw.run)
 }
 
 func (pw *Visualizer) Update(t screen.Texture) {
-	pw.tx <- t
+  pw.tx <- t
 }
 
 func (pw *Visualizer) run(s screen.Screen) {
-	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title:  pw.Title,
-		Width:  800,
-		Height: 800,
-	})
-	w.Send(paint.Event{})
-	if err != nil {
-		log.Fatal("Failed to initialize the app window:", err)
-	}
-	defer func() {
-		w.Release()
-		close(pw.done)
-	}()
+  w, err := s.NewWindow(&screen.NewWindowOptions{
+    Title:  pw.Title,
+    Width:  800,
+    Height: 800,
+  })
+  w.Send(paint.Event{})
+  if err != nil {
+    log.Fatal("Failed to initialize the app window:", err)
+  }
+  defer func() {
+    w.Release()
+    close(pw.done)
+  }()
 
-	if pw.OnScreenReady != nil {
-		pw.OnScreenReady(s)
-	}
+  if pw.OnScreenReady != nil {
+    pw.OnScreenReady(s)
+  }
 
-	pw.w = w
+  pw.w = w
 
-	events := make(chan any)
-	go func() {
-		for {
-			e := w.NextEvent()
-			if pw.Debug {
-				log.Printf("new event: %v", e)
-			}
-			if detectTerminate(e) {
-				close(events)
-				break
-			}
-			events <- e
-		}
-	}()
+  events := make(chan any)
+  go func() {
+    for {
+      e := w.NextEvent()
+      if pw.Debug {
+        log.Printf("new event: %v", e)
+      }
+      if detectTerminate(e) {
+        close(events)
+        break
+      }
+      events <- e
+    }
+  }()
 
-	var t screen.Texture
+  var t screen.Texture
 
-	for {
-		select {
-		case e, ok := <-events:
-			if !ok {
-				return
-			}
-			pw.handleEvent(e, t)
+  for {
+    select {
+    case e, ok := <-events:
+      if !ok {
+        return
+      }
+      pw.handleEvent(e, t)
 
-		case t = <-pw.tx:
-			w.Send(paint.Event{})
-		}
-	}
+    case t = <-pw.tx:
+      w.Send(paint.Event{})
+    }
+  }
 }
 
 func detectTerminate(e any) bool {
-	switch e := e.(type) {
-	case lifecycle.Event:
-		if e.To == lifecycle.StageDead {
-			return true // Window destroy initiated.
-		}
-	case key.Event:
-		if e.Code == key.CodeEscape {
-			return true // Esc pressed.
-		}
-	}
-	return false
+  switch e := e.(type) {
+  case lifecycle.Event:
+    if e.To == lifecycle.StageDead {
+      return true // Window destroy initiated.
+    }
+  case key.Event:
+    if e.Code == key.CodeEscape {
+      return true // Esc pressed.
+    }
+  }
+  return false
 }
 
 func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
-	switch e := e.(type) {
+  switch e := e.(type) {
 
-	case size.Event: // Оновлення даних про розмір вікна.
-		pw.sz = e
+  case size.Event: // Оновлення даних про розмір вікна.
+    pw.sz = e
 
-	case error:
-		log.Printf("ERROR: %s", e)
+  case error:
+    log.Printf("ERROR: %s", e)
 
-	case mouse.Event:
-		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
-		}
+  case mouse.Event:
+    if t == nil {
+      switch e.Button {
+      case mouse.ButtonLeft:
+        if e.Direction == mouse.DirPress {
+          cross.move(int(e.X), int(e.Y))
+          fmt.Println("X:", e.X, "Y:", e.Y)
+          pw.w.Send(paint.Event{})
+        }
+      }
+    }
 
-	case paint.Event:
-		// Малювання контенту вікна.
-		if t == nil {
-			pw.drawDefaultUI()
-		} else {
-			// Використання текстури отриманої через виклик Update.
-			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
-		}
-		pw.w.Publish()
-	}
+  case paint.Event:
+    // Малювання контенту вікна.
+    if t == nil {
+      pw.drawDefaultUI()
+    } else {
+      // Використання текстури отриманої через виклик Update.
+      pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
+    }
+    pw.w.Publish()
+  }
 }
 
 func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
-	pw.w.Fill(image.Rect(200, 350, 600, 450), color.RGBA{255, 255, 0, 255}, draw.Src)
-	pw.w.Fill(image.Rect(350, 200, 450, 600), color.RGBA{255, 255, 0, 255}, draw.Src)
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+  pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+  cross.drawCross(pw)
 
-	// Малювання білої рамки.
-	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
-		pw.w.Fill(br, color.White, draw.Src)
-	}
+  // Малювання білої рамки.
+  for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
+    pw.w.Fill(br, color.White, draw.Src)
+  }
 }
